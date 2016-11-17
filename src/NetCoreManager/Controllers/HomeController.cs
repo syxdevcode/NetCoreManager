3f7779a11f1b31f5e269caf64978652646d30470
syxtest01@gmail.com <xing_*666888>
2016-11-17 11:37:03 +0800

    修改项目命名

--- src/NetCoreManager/Controllers/HomeController.cs
+++ src/NetCoreManager/Controllers/HomeController.cs
@@ -4,30 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace NetCoreManager.Controllers
+namespace NetCoreManager.Mvc.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
         {
             return View();
         }
--- src/NetCoreManager/Program.cs
+++ src/NetCoreManager/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace NetCoreManager
+namespace NetCoreManager.Mvc
 {
     public class Program
     {
