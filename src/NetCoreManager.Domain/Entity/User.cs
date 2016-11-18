7b6bdc81dfc91d6ebc69be305abbe73aa899e125
syxtest01@gmail.com <xing_*666888>
2016-11-18 08:38:37 +0800

    构建框架

---
+++ src/NetCoreManager.Domain/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreManager.Domain.Entity
+{
+    public class User: IAggregateRoot
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AddTime { get; set; }
+
+        public bool IsDelete { get; set; }
+    }
+}
---
+++ src/NetCoreManager.Repository/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetCoreManager.Repository")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1de04240-283c-45fd-800d-3f15d5aff6dd")]
