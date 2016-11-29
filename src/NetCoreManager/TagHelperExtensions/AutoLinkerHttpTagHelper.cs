5c21343411cfe74ba00780bd687a1e21c8c4ce37
syxtest01@gmail.com <xing_*666888>
2016-11-29 17:26:18 +0800

    添加http和www的锚链接

---
+++ src/NetCoreManager/TagHelperExtensions/AutoLinkerHttpTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NetCoreManager.Mvc.TagHelperExtensions
+{
+    [HtmlTargetElement("p")]
+    public class AutoLinkerHttpTagHelper:TagHelper
+    {
+        // This filter must run before the AutoLinkerWwwTagHelper as it searches and replaces http and 
+        // the AutoLinkerWwwTagHelper adds http to the markup.
+        //public override int Order
+        //{
+        //    get { return int.MinValue; }
+        //}
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = output.Content.IsModified ? output.Content.GetContent() :
+                (await output.GetChildContentAsync()).GetContent();
+
+            // Find Urls in the content and replace them with their anchor tag equivalent.
+            output.Content.SetHtmlContent(Regex.Replace(
+                 childContent,
+                 @"\b(?:https?://)(\S+)\b",
+                  "<a target=\"_blank\" href=\"$0\">$0</a>"));  // http link version}
+        }
+    }
+}
